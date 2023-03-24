4a9462de09e28ad7de11759e057452b1e02da359
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-03-24 16:42:06 -0300

    Se crea la función para agregarle texto a medida que se va cambiando el componente de lugar hacia la derecha

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,40 @@ let ultimaCancion = arraySongs[arraySongs.length - 1];
 const btnLeft = document.querySelector('.btn-left');
 const btnrigth = document.querySelector('.btn-rigth');
 slider.insertAdjacentElement('afterbegin', ultimaCancion);
+let topicCounter = 0;
+
+/**Acá creo el array que va a contener el  */
+const arraySongsName = [
+    {
+        name: 'High Hopes',
+        artists: 'Panic at the Disco'
+    },
+    {
+        name: 'Payphone',
+        artists: 'Maroon 5 & Wiz Khalifa'
+    },
+    {
+        name: 'Lose Yourself',
+        artists: 'Eminem'
+    },
+    {
+        name: 'Past Life',
+        artists: 'Trevor Daniel & Selena Gomez'
+    }
+]
+/**Acá creo la función para poder cambiar al siguiente tema y también para volver al anterior */
+let songDescription = document.querySelector('.description-song-container')
+function colocarTexto(){
+    const songDescription1 = `
+        <span class="name-song">
+            ${arraySongsName[topicCounter].name}
+        </span>
+        <span class="description-song">
+            ${arraySongsName[topicCounter].artists}
+        </span>
+    `
+    songDescription.innerHTML = songDescription1
+}
 
 const next=()=>{
     let primerCanción = document.querySelectorAll('.song-icon-container')[0];
@@ -13,7 +47,13 @@ const next=()=>{
         slider.style.transition = 'none';
         slider.insertAdjacentElement('beforeend', primerCanción);
         slider.style.marginLeft = '-71%'
-    },100)
+    },100);
+    if (topicCounter >=0 && topicCounter < 3){
+        topicCounter++
+    } else {
+        topicCounter = 0
+    }
+    colocarTexto();
 }
 const back=()=>{
     let primerCanción = document.querySelectorAll('.song-icon-container')[arraySongs.length -1]
@@ -24,53 +64,23 @@ const back=()=>{
         slider.insertAdjacentElement('afterbegin', primerCanción);
         slider.style.marginLeft = '-150.5%'
     },100)
+    topicCounter--
+    colocarTexto();
 }
 
 btnrigth.addEventListener('click', next);
 btnLeft.addEventListener('click', back);
-
-
-// margin-left: -70%;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**Acá hago lo del crecimiento de la barrita */
 
 const barrita = document.querySelector('.progressbar-barra');
 let progreso = 0;
 const interval = setInterval(() => {
-    if (progreso >= 100) {
-        clearInterval(interval);
-    } else {
-        progreso++;
-        barrita.style.width = `${progreso}%`;
-    }
+if (progreso >= 100) {
+clearInterval(interval);
+} else {
+progreso++;
+barrita.style.width = `${progreso}%`;
+}
 }, 1000);
+
+
