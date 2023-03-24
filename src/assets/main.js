ad9b541b466736c14e2f532c7d5198d6b9254a92
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-03-24 10:44:50 -0300

    Terminada la sección de botones del reproductor. queda comenzar a trabajar en las funciones de cada uno e integrarle los respectivos temas al reproductor

--- src/assets/main.js
+++ src/assets/main.js
@@ -64,13 +64,13 @@ btnLeft.addEventListener('click', back);
 
 
 
-// const barrita = document.querySelector('.progressbar-barra');
-// let progreso = 0;
-// const interval = setInterval(() => {
-//     if (progreso >= 100) {
-//         clearInterval(interval);
-//     } else {
-//         progreso++;
-//         barrita.style.width = `${progreso}%`;
-//     }
-// }, 1000);
+const barrita = document.querySelector('.progressbar-barra');
+let progreso = 0;
+const interval = setInterval(() => {
+    if (progreso >= 100) {
+        clearInterval(interval);
+    } else {
+        progreso++;
+        barrita.style.width = `${progreso}%`;
+    }
+}, 1000);
